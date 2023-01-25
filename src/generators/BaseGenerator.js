14ac5539f24a5e6fa1bc47046bd0e50fa23111ce
ToKruAmApp <77840536+ToKruAmApp@users.noreply.github.com>
2023-01-25 15:27:22 +0100

    Add case for schema accessCode

--- src/generators/BaseGenerator.js
+++ src/generators/BaseGenerator.js
@@ -136,6 +136,9 @@ export default class {
       case "http://schema.org/url":
         return { type: "url" };
 
+      case "http://schema.org/accessCode":
+        return { type: "password" };
+
       case this.entrypointWithSlash + "color": // fall through
       case "http://schema.org/color":
         return { type: "color" };
